4f3a6bcb72881260505384e6319ff431d3981efb
Eduardo Ballesteros Abellán <ballesteros_eba@hotmail.com>
2019-11-19 18:01:10 +0100

    Differential entropy tested and rename of test_differential_entropy to avoid confussions with other tests

--- test_differential_entropy.py
+++ test_differential_entropy.py
@@ -8,8 +8,10 @@ x_max = 1
 
 
 def f(x):
-    if (x > x_min) & (x < x_max):
-        return 1
+    if (x > x_min) & (x < x_max/2):
+        return 0.5
+    elif (x > x_max/2) & (x < x_max):
+        return 1.5
     else:
         return 0
 
