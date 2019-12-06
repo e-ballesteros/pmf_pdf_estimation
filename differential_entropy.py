29acc912f0c553d64566906da81973ed7cc087c7
Eduardo Ballesteros Abellán <ballesteros_eba@hotmail.com>
2019-12-06 19:10:02 +0100

    Added comments to differential_entropy and minor changes to pdf_gaussian

--- differential_entropy.py
+++ differential_entropy.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
 
-from numpy import inf                       # We will need to integrate with infinite limits
 from numpy import log as ln
 from numpy import trapz
 
 
-def differential_entropy(pdf, x_pdf):       # pdf is a vector because we want to perform a numerical integration
+def differential_entropy(pdf, x_pdf):           # pdf is a vector because we want to perform a numerical integration
 
-    f = []
+    f = []                                      # Vector we want to integrate with the numerical integration function
 
     for i in range(0, len(pdf)):
-        if pdf[i] > 0:
+        if pdf[i] > 0:                          # To avoid ln of 0 operation
             f.append(-1 * pdf[i] * ln(pdf[i]))
         else:
             f.append(0)
 
-    ans = trapz(f, x_pdf)
+    ans = trapz(f, x_pdf)                       # Integrate using the composite trapezoidal rule
     return ans
--- pdf_gaussian.py
+++ pdf_gaussian.py
@@ -3,6 +3,7 @@
 from math import pi
 from math import exp
 
+
 # It returns the probability given a certain value of x and the mu and sigma of the gaussian distribution
 def gaussian_pdf(x, mu, sigma):
     return 1.0 / (sigma * (2.0 * pi)**(1/2)) * exp(-1.0 * (x - mu)**2 / (2.0 * (sigma**2)))
