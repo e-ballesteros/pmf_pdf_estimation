a7018218f0c479b6b6da55b4d93247d11a0ebe92
Eduardo Ballesteros Abellán <ballesteros_eba@hotmail.com>
2019-12-06 19:16:23 +0100

    Added comments to estimation_pmf and changed N variable to n

--- estimation_pmf.py
+++ estimation_pmf.py
@@ -3,7 +3,8 @@
 import numpy as np
 
 
+# Function that computes a pmf_vector given a list of samples
 def estimate_pmf(samples):
-    N = len(samples)
-    samples_list, pmf_vector = np.unique(samples, return_counts=True)
-    return samples_list, pmf_vector/N
+    n = len(samples)
+    samples_list, pmf_vector = np.unique(samples, return_counts=True)   # Counts unique elements in samples list
+    return samples_list, pmf_vector/n                                   # Returns pmf vector and the samples list
